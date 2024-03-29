bbd0b06babe11c39c41011932b20973d035aaddf
Samuel Magana <maganasamuel001@gmail.com>
2024-03-29 21:31:10 +0800

    Check auth guard redirection

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,21 +2,16 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
-
 class HomeController extends Controller
 {
     public function index()
     {
         if (auth()->check()) {
-            if (auth()->user()->isAn('admin')) {
-                return redirect(route('admin.contests.index'));
-            }
+            return redirect(route('admin.contests.index'));
+        }
 
-            if (auth()->user()->isA('judge')) {
-                return redirect(route('judge.categories.index'));
-            }
+        if (auth('judge')->check()) {
+            return redirect(route('judge.categories.index'));
         }
 
         return redirect(route('login'));
