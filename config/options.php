54838f728a67fafbbd078cc79bf4a5faf44cd49c
Samuel Magana <maganasamuel001@gmail.com>
2024-03-29 23:12:10 +0800

    Configure scoring systems and category statuses options

---
+++ config/options.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'scoring_systems' => [
+        'average' => 'Average',
+        'ranking' => 'Ranking',
+    ],
+    'category_statuses' => [
+        'que' => 'Pending',
+        'scoring' => 'Scoring',
+        'done' => 'Completed',
+    ],
+];
