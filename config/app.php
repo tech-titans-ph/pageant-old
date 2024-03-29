a1e32df5df1127d42e406a05a9481e8fe01619f9
Samuel Magana <maganasamuel001@gmail.com>
2024-03-29 21:12:58 +0800

    Remove bouncer package

--- config/app.php
+++ config/app.php
@@ -1,7 +1,6 @@
 <?php
 
 return [
-
     /*
     |--------------------------------------------------------------------------
     | Application Name
@@ -135,7 +134,6 @@ return [
      */
 
     'providers' => [
-
         // Laravel Framework Service Providers...
         Illuminate\Auth\AuthServiceProvider::class,
         Illuminate\Broadcasting\BroadcastServiceProvider::class,
@@ -168,9 +166,6 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-
-        Silber\Bouncer\BouncerServiceProvider::class,
-
     ],
 
     /*
@@ -185,7 +180,6 @@ return [
      */
 
     'aliases' => [
-
         'App' => Illuminate\Support\Facades\App::class,
         'Arr' => Illuminate\Support\Arr::class,
         'Artisan' => Illuminate\Support\Facades\Artisan::class,
@@ -221,9 +215,5 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-
-        'Bouncer' => Silber\Bouncer\BouncerFacade::class,
-
     ],
-
 ];
