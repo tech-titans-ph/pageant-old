4a2a3ce329931c394e01dd2a148da4648b450b0c
Samuel Magana <maganasamuel001@gmail.com>
2024-03-30 10:33:33 +0800

    Make logo nullable

--- database/migrations/2019_05_28_032447_create_contests_table.php
+++ database/migrations/2019_05_28_032447_create_contests_table.php
@@ -17,7 +17,7 @@ class CreateContestsTable extends Migration
             $table->string('name');
             $table->string('description');
             $table->string('scoring_system');
-            $table->string('logo');
+            $table->text('logo')->nullable();
 
             $table->timestamps();
         });
