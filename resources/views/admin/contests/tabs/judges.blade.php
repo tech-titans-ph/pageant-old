a34f30a91ae47edc9b8deb4820124de7ec4970fe
Samuel Magana <maganasamuel001@gmail.com>
2024-03-31 00:10:59 +0800

    Update input picker for judges

--- resources/views/admin/contests/tabs/judges.blade.php
+++ resources/views/admin/contests/tabs/judges.blade.php
@@ -5,20 +5,17 @@
         action="{{ route('admin.contests.judges.store', ['contest' => $contest->id, 'activeTab' => 'Judges']) }}"
         class="flex flex-wrap items-start lg:flex-no-wrap">
         @csrf
+
         @formField(['error' => 'name', 'class' => 'block flex-grow lg:mr-4 mb-6 lg:mb-0'])
         <div>
           <input-picker api="{{ route('admin.judges.index') }}"
-            hidden-name="user_id"
+            hidden-name="name"
             display-name="name"
-            hidden-property="id"
+            hidden-property="name"
             display-property="name"
-            hidden-value="{{ old('user_id') ?? '' }}"
-            display-value="{{ old('name') ?? '' }}"
+            hidden-value="{{ old('name') }}"
+            display-value="{{ old('name') }}"
             placeholder="Enter name of judge..."></input-picker>
-
-          @error('user_id')
-            <div class="mt-4 text-xs italic text-red-500">{{ $message }}</div>
-          @enderror
         </div>
         @endformField
 
@@ -27,6 +24,7 @@
         @endbutton
       </form>
     </li>
+
     @forelse ($contest->judges as $judge)
       <li class="p-4 border-t">
         <div class="flex flex-col items-start lg:flex-row lg:items-center">
@@ -38,11 +36,13 @@
             @buttonLink(['href' => route('admin.contests.judges.login', ['contest' => $contest->id, 'judge' => $judge->id]), 'class' => 'mr-2'])
               Login
             @endbuttonLink
+            
             <form method="post"
               action="{{ route('admin.contests.judges.destroy', ['contest' => $contest->id, 'judge' => $judge->id]) }}"
               class="inline-block btn">
               @csrf
               @method('DELETE')
+
               @button(['type' => 'submit', 'color' => 'danger'])
               Remove
               @endbutton
--- resources/views/admin/judges/edit.blade.php
+++ resources/views/admin/judges/edit.blade.php
@@ -1,40 +1,36 @@
 @extends('layouts.admin')
+
 @section('content')
-	@breadcrumb([
-		'links' => [
-			['url' => route('admin.contests.index'), 'title' => 'Contests'],
-			['url' => route('admin.contests.show', ['contest' => $contest->id, 'activeTab' => 'Judges']), 'title' => $contest->name],
-		],
-		'class' => 'mb-10',
-	])
-		@pageHeader()
-			Edit Judge
-		@endpageHeader
-	@endbreadcrumb
-	<div class="flex">
-		<div class="w-3/4">
-			@card
-				<form method="post" action="{{ route('admin.contests.judges.update', ['contest' => $contest->id, 'judge' => $judge->id]) }}">
-					@csrf
-					@method('PATCH')
-					@formField(['label' => 'Name', 'error' => 'name'])
-						<input-picker
-							api="{{ route('admin.judges.index') }}"
-							hidden-name="user_id"
-							display-name="name"
-							hidden-property="id"
-							display-property="name"
-							hidden-value="{{ old('user_id') ?? $judge->user_id }}"
-							display-value="{{ old('name') ?? $judge->user->name }}"
-						></input-picker>
-						
-						@error('user_id')
-							<div class="text-red-500 text-xs italic mt-4">{{ $message }}</div>
-						@enderror
-					@endformField
-					@button(['type' => 'submit']) Edit @endbutton
-				</form>
-			@endcard
-		</div>
-	</div>
+  @breadcrumb([
+      'links' => [['url' => route('admin.contests.index'), 'title' => 'Contests'], ['url' => route('admin.contests.show', ['contest' => $contest->id, 'activeTab' => 'Judges']), 'title' => $contest->name]],
+      'class' => 'mb-10'
+  ])
+    @pageHeader()
+      Edit Judge
+    @endpageHeader
+  @endbreadcrumb
+
+  <div class="flex">
+    <div class="w-3/4">
+      @card
+        <form method="post"
+          action="{{ route('admin.contests.judges.update', ['contest' => $contest->id, 'judge' => $judge->id]) }}">
+          @csrf
+          @method('PATCH')
+
+          @formField(['label' => 'Name', 'error' => 'name'])
+          <input-picker api="{{ route('admin.judges.index') }}"
+            hidden-name="name"
+            display-name="name"
+            hidden-property="name"
+            display-property="name"
+            hidden-value="{{ old('name', $judge->name) }}"
+            display-value="{{ old('name', $judge->name) }}"></input-picker>
+          @endformField
+
+          @button(['type' => 'submit']) Edit @endbutton
+        </form>
+      @endcard
+    </div>
+  </div>
 @endsection
