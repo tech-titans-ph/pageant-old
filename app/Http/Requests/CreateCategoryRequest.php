4a842ba3bc42615abd848474503f89ff19bb9825
Samuel Magana <maganasamuel001@gmail.com>
2024-03-30 20:06:33 +0800

    Update rules

--- app/Http/Requests/CreateCategoryRequest.php
+++ app/Http/Requests/CreateCategoryRequest.php
@@ -27,8 +27,25 @@ class CreateCategoryRequest extends FormRequest
         $contest = $this->route('contest');
 
         return [
-            'name' => ['required', 'string', 'max:255', Rule::unique('categories')->where('contest_id', $contest->id)],
-            'percentage' => ['required', 'integer', 'min:1', 'max:100'],
+            'name' => [
+                'required',
+                'string',
+                'max:255',
+                Rule::unique('categories')->where('contest_id', $contest->id),
+            ],
+            'has_criterias' => ['boolean'],
+            'scoring_system' => [
+                'nullable',
+                'required_if:has_criterias,true',
+                Rule::in($contest->scoring_system == 'ranking' ? array_keys(config('options.scoring_systems')) : 'average'),
+            ],
+            'max_points_percentage' => [
+                'nullable',
+                Rule::requiredIf(! ($this->has_criterias && ($this->scoring_system == 'ranking' || $contest->scoring_system == 'ranking'))),
+                'integer',
+                'min:2',
+                'max:100',
+            ],
         ];
     }
 
