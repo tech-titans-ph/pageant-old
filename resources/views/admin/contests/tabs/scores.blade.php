75b5398f4b93d90ff361a74f673a1b6242cb3fcb
Samuel Magana <maganasamuel001@gmail.com>
2024-04-01 00:20:59 +0800

    Display correct contestant scores

--- resources/views/admin/contests/tabs/scores.blade.php
+++ resources/views/admin/contests/tabs/scores.blade.php
@@ -6,21 +6,14 @@
       @endbuttonLink
     </li>
 
-    @php
-      $top = 0;
-    @endphp
-
-    @forelse($scoredContestants as $contestant)
-      @php
-        $top++;
-      @endphp
+    @forelse($contest->ranked_contestants as $contestant)
       <li class="p-4 border-t">
         <a href="{{ route('admin.contests.contestants.show', ['contest' => $contest->id, 'contestant' => $contestant->id]) }}"
           class="flex flex-wrap lg:flex-no-wrap">
           <div class="flex-none">
             <img src="{{ $contestant->avatar_url }}"
               class="object-cover object-center w-64 h-64 mx-auto border rounded-full lg:w-32 lg:h-32 lg:ml-0 lg:mr-2">
-            <div class="mt-1 text-sm font-medium text-center">Top {{ $top }}</div>
+            <div class="mt-1 text-sm font-medium text-center">Top {{ $contestant->ranking }}</div>
           </div>
           <div class="self-center flex-grow mt-4 lg:mt-0">
             <div class="block font-bold">
@@ -30,9 +23,12 @@
               {{ $contestant->alias }}
             </div>
           </div>
-          <div class="self-center flex-none w-full text-6xl font-bold text-center text-green-700 whitespace-no-wrap lg:pl-2 lg:w-auto">
-            {{ round($contestant->totalPercentage, 4) }}
-          </div>
+
+          @if ($contest->scoring_system == 'average')
+            <div class="self-center flex-none w-full text-6xl font-bold text-center text-green-700 whitespace-no-wrap lg:pl-2 lg:w-auto">
+              {{ round($contestant->average_sum, 4) }}
+            </div>
+          @endif
         </a>
       </li>
     @empty
