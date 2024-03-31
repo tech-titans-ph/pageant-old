42fa7df2566ee65eb541a2b8c41fc83384b7053f
Samuel Magana <maganasamuel001@gmail.com>
2024-03-31 10:17:10 +0800

    Use category form script

--- resources/views/admin/contests/show.blade.php
+++ resources/views/admin/contests/show.blade.php
@@ -51,44 +51,4 @@
   </div>
 @endsection
 
-@push('scripts')
-  <script type="text/javascript">
-    window.addEventListener('load', () => {
-      let hasCriterias = document.querySelector('#has_criterias');
-
-      let scoringSystem = document.querySelector('#scoring_system');
-
-      let maxPointsPercentage = document.querySelector('#max_points_percentage');
-
-      let scoringSystemWrapper = document.querySelector('.scoring-system-wrapper');
-
-      let maxPointsPercentageWrapper = document.querySelector('.max-points-percentage-wrapper');
-
-      let contestScoringSystem = "{{ $contest->scoring_system }}";
-
-      hasCriterias.addEventListener('change', function(event) {
-        scoringSystem.value = '';
-
-        scoringSystemWrapper.classList.add('hidden');
-
-        if (contestScoringSystem == 'ranking') {
-          maxPointsPercentageWrapper.classList.remove('hidden');
-        }
-
-        if (event.target.checked) {
-          scoringSystemWrapper.classList.remove('hidden');
-        }
-      });
-
-      scoringSystem.addEventListener('change', function(event) {
-        maxPointsPercentageWrapper.classList.add('hidden');
-
-        if (event.target.value == 'average') {
-          maxPointsPercentageWrapper.classList.remove('hidden');
-        } else {
-          maxPointsPercentage.value = '';
-        }
-      });
-    });
-  </script>
-@endpush
+@include('admin.categories.form-script')
