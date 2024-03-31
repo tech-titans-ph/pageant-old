3c28d527132667356cbfeaf016a94d5621f13b37
Samuel Magana <maganasamuel001@gmail.com>
2024-03-31 13:36:23 +0800

    Toggle max points percentage wrapper

--- resources/views/admin/categories/form-script.blade.php
+++ resources/views/admin/categories/form-script.blade.php
@@ -18,24 +18,26 @@
 
         scoringSystemWrapper.classList.add('hidden');
 
-        if (contestScoringSystem == 'ranking') {
-          maxPointsPercentageWrapper.classList.remove('hidden');
-        }
-
         if (event.target.checked) {
           scoringSystemWrapper.classList.remove('hidden');
         }
+
+        toggleMaxPointsPercentageWrapper();
       });
 
       scoringSystem.addEventListener('change', function(event) {
+        toggleMaxPointsPercentageWrapper();
+      });
+
+      function toggleMaxPointsPercentageWrapper() {
         maxPointsPercentageWrapper.classList.add('hidden');
 
-        if (event.target.value == 'average') {
+        if ((contestScoringSystem == 'ranking' && !hasCriterias.checked) || contestScoringSystem == 'average') {
           maxPointsPercentageWrapper.classList.remove('hidden');
         } else {
           maxPointsPercentage.value = '';
         }
-      });
+      }
     });
   </script>
 @endpush
