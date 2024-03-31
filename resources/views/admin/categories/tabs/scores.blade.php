848faefd73dcc76a86d88904fbcc3c898b20e3b9
Samuel Magana <maganasamuel001@gmail.com>
2024-03-31 23:03:29 +0800

    Revised display ranked category contestanted

--- resources/views/admin/categories/tabs/scores.blade.php
+++ resources/views/admin/categories/tabs/scores.blade.php
@@ -13,33 +13,28 @@
         @endbuttonLink
       @endif
     </li>
-    @php
-      $top = 0;
-    @endphp
-    @foreach ($scoredCategoryContestants as $contestant)
-      @php
-        $top++;
-      @endphp
+
+    @foreach ($category->ranked_contestants as $contestant)
       <li class="p-4 border-t">
         <div class="flex justify-between mx-auto">
           <div class="flex flex-grow">
             <div class="flex-none">
               <img src="{{ $contestant->avatar_url }}"
                 class="object-cover object-center w-32 h-32 border rounded-full">
-              <div class="mt-1 text-sm font-medium text-center">Top {{ $top }}</div>
+              <div class="mt-1 text-sm font-medium text-center">Top {{ $contestant->ranking }}</div>
             </div>
-            <a href="{{ route('admin.contests.categories.category-contestants.show', ['contest' => $contest->id, 'category' => $category->id, 'categoryContestant' => $contestant->id]) }}"
+            <a href="{{ route('admin.contests.categories.contestants.show', ['contest' => $contest->id, 'category' => $category->id, 'contestant' => $contestant->id]) }}"
               class="flex flex-col self-center px-4">
               <div class="block mb-2 font-bold">
-                # {{ $contestant->contestant->number . ' - ' . $contestant->contestant->name }}
+                # {{ $contestant->order . ' - ' . $contestant->name }}
               </div>
               <div class="italic">
-                {{ $contestant->contestant->description }}
+                {{ $contestant->alias }}
               </div>
             </a>
           </div>
           <div class="self-center text-6xl font-bold text-green-700 whitespace-no-wrap">
-            {{ round($contestant->averagePercentage, 4) }}
+            {{ round($contestant->average, 4) }}
           </div>
         </div>
       </li>
