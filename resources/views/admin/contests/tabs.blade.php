720a2cf919959e3a1f927bb630d0059c13f28826
maganasamuel <maganasamuel001@gmail.com>
2024-04-01 15:44:35 +0800

    Add condition when showing contest scores and create contest from scores tabs

--- resources/views/admin/contests/tabs.blade.php
+++ resources/views/admin/contests/tabs.blade.php
@@ -3,7 +3,9 @@
   @include('admin.contests.tabs.contestants')
   @include('admin.contests.tabs.categories')
 
-  @if (!$contest->categories()->whereIn('status', ['que', 'scoring'])->count())
+  @if (
+      $contest->categories()->count() &&
+          !$contest->categories()->whereIn('status', ['que', 'scoring'])->count())
     @include('admin.contests.tabs.scores')
     @include('admin.contests.tabs.create-contest-from-results')
   @endif
