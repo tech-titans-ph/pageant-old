bc0dc6b302a7e2bcfdb9f009df87b6b308750c6e
maganasamuel <maganasamuel001@gmail.com>
2024-04-02 13:07:32 +0800

    Correct img tag

--- resources/views/admin/scores/category.blade.php
+++ resources/views/admin/scores/category.blade.php
@@ -56,7 +56,7 @@
             @if (!$judgeKey)
               <td class="px-2 py-1 text-center align-top border whitespace-nowrap"
                 rowspan="{{ $category->judges->count() }}">
-                <image src="{{ $contestant->avatar_url }}"
+                <img src="{{ $contestant->avatar_url }}"
                   class="object-cover object-center w-32 h-32 mx-auto mb-1 border rounded-full" />
                 <div>Top {{ $contestant->ranking }}</div>
               </td>
