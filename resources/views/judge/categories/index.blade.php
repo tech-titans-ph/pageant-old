29d7065f9d855c6abf1641dcb4cc58ce0ac87ea0
Samuel Magana <maganasamuel001@gmail.com>
2024-04-03 07:50:40 +0800

    Update mobile layout

--- resources/views/judge/categories/index.blade.php
+++ resources/views/judge/categories/index.blade.php
@@ -1,28 +1,35 @@
 @extends('layouts.mobile')
 @section('navbar-right')
-	<a href="{{ route('logout') }}" class="px-4 flex justify-center items-center no-underline block h-full hover:bg-gray-200" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-		Logout
-	</a>
-	<form id="logout-form" action="{{ route('logout') }}" method="POST" class="hidden">
-		{{ csrf_field() }}
-	</form>
+  <a href="{{ route('logout') }}"
+    class="flex items-center justify-center block h-full px-4 no-underline hover:bg-gray-200"
+    onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+    Logout
+  </a>
+  <form id="logout-form"
+    action="{{ route('logout') }}"
+    method="POST"
+    class="hidden">
+    {{ csrf_field() }}
+  </form>
 @endsection
 @section('content')
-	<div class="mx-auto pt-12">
-		<div class="border-b p-4 text-center leading-normal">
-			<h2 class="text-lg font-bold">{{ $judge->contest->name }}</h2>
-			<p class="italic font-thin text-sm">Judge: {{ $judge->user->name }}</p>
-			<p class="text-sm">Please select a Category below to start scoring.</p>
-		</div>
-		<div class="md:rounded md:shadow-md mx-auto">
-			<div class="md:flex justify-center md:justify-between border-t">
-				<div class="w-full md:w-1/2 mx-auto">
-					<img src="{{ Storage::url($judge->contest->logo) }}" class="w-full object-contain object-center" alt="Contestant">
-				</div>
-			</div>
-			
-			<judge-category api="{{ route('judge.categories.list-categories') }}"></judge-category>
-		</div>
-	</div>
-	<alert-judge api="{{ route('judge.categories.status') }}"></alert-judge>
+  <div class="mx-auto">
+    <div class="p-4 leading-normal text-center border-b">
+      <h2 class="text-lg font-bold">{{ $judge->contest->name }}</h2>
+      <p class="text-sm italic font-thin">Judge: {{ $judge->user->name }}</p>
+      <p class="text-sm">Please select a Category below to start scoring.</p>
+    </div>
+    <div class="mx-auto md:rounded md:shadow-md">
+      <div class="justify-center border-t md:flex md:justify-between">
+        <div class="w-full mx-auto md:w-1/2">
+          <img src="{{ Storage::url($judge->contest->logo) }}"
+            class="object-contain object-center w-full"
+            alt="Contestant">
+        </div>
+      </div>
+
+      <judge-category api="{{ route('judge.categories.list-categories') }}"></judge-category>
+    </div>
+  </div>
+  <alert-judge api="{{ route('judge.categories.status') }}"></alert-judge>
 @endsection
--- resources/views/layouts/mobile.blade.php
+++ resources/views/layouts/mobile.blade.php
@@ -21,25 +21,30 @@
 </head>
 
 <body class="font-sans antialiased leading-none">
-  <div id="app">
-    <div class="fixed flex items-center justify-between w-full h-12 bg-white shadow">
+  <div id="app"
+    class="flex flex-col h-screen">
+    <div class="flex items-center justify-between flex-none w-full bg-white shadow">
       <div class="flex-none">
         <a href="{{ route('judge.categories.index') }}"
-          class="flex items-center justify-center block w-12 h-12 no-underline hover:bg-gray-200">
+          class="flex items-center justify-center w-12 h-12 no-underline hover:bg-gray-200">
           @svg('home-solid', 'w-6 h-6 fill-current')
         </a>
       </div>
       <div class="flex items-center justify-center flex-grow font-normal text-center">
-        <img src="{{ asset('images/logo.png') }}"
-          class="inline-block h-8 mr-2 rounded-full">
-        <div>
-          Contest<span class="font-bold">Hub</span>
+        <div class="flex items-center justify-end h-24 max-h-24">
+          <img src="{{ asset('images/logo.png') }}"
+            class="object-contain h-40 ">
+        </div>
+        <div class="-ml-8 font-bold">
+          {{ config('app.name') }}
         </div>
       </div>
       @yield('navbar-right')
     </div>
 
-    @yield('content')
+    <div class="flex-grow overflow-y-auto">
+      @yield('content')
+    </div>
   </div>
 
   <!-- Scripts -->
