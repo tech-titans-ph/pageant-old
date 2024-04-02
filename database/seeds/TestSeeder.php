d3d9cecffade640ab4a313c36afe529b5be7c2a9
maganasamuel <maganasamuel001@gmail.com>
2024-04-02 11:52:44 +0800

    Delete mocked contest data

--- database/seeds/TestSeeder.php
+++ database/seeds/TestSeeder.php
@@ -13,14 +13,17 @@ class TestSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
-        Artisan::call('migrate:fresh --seed');
+        /* $this->deleteContest(2);
+        return; */
 
-        $this->scenario('average', true, 'average');
+        // Artisan::call('migrate:fresh --seed');
+
+        // $this->scenario('average', true, 'average');
         // $this->scenario('average');
 
         // $this->scenario('ranking', true, 'average');
 
-        // $this->scenario('ranking', true, 'ranking');
+        $this->scenario('ranking', true, 'ranking');
         // $this->scenario('ranking');
 
         return;
@@ -114,115 +117,26 @@ class TestSeeder extends Seeder
         });
     }
 
-    public function oldTest(Faker $faker)
+    public function deleteContest($id)
     {
-        Storage::deleteDirectory('logos');
-        Storage::deleteDirectory('profile-pictures');
+        $contest = Contest::find($id);
 
-        User::truncate();
-        Contest::truncate();
-        Contestant::truncate();
-        Judge::truncate();
-        Category::truncate();
-        Criteria::truncate();
-        CategoryContestant::truncate();
-        CategoryJudge::truncate();
-        CategoryScore::truncate();
-        CriteriaScore::truncate();
+        $contest->categories()->get()->each(function ($category) {
+            $category->scores()->delete();
 
-        $judgeCount = 3;
+            $category->criterias()->delete();
 
-        $contestantCount = 3;
+            $category->judges()->detach($category->judges()->pluck('judges.id'));
 
-        $categoryCount = 3;
+            $category->contestants()->detach($category->contestants()->pluck('contestants.id'));
 
-        $criteriaPerCategoryCount = 3;
+            $category->delete();
+        });
 
-        $status = 'scoring'; // que, scoring, done
+        $contest->judges()->delete();
 
-        $admin = factory(User::class)->states('admin')->create(['name' => 'Administrator', 'username' => 'admin']);
+        $contest->contestants()->delete();
 
-        $contest = factory(Contest::class)->create();
-
-        $contest->judges()->createMany(
-            factory(Judge::class, $judgeCount)->make(['contest_id' => $contest->id])->toArray()
-        );
-
-        foreach (range(1, $contestantCount) as $i) {
-            factory(Contestant::class)->create(['contest_id' => $contest->id, 'number' => $i]);
-        }
-
-        foreach (range(1, $categoryCount) as $i) {
-            $percentage = $faker->numberBetween(1, floor(100 / $categoryCount));
-
-            if ($i === $categoryCount) {
-                $percentage = 100 - $contest->categories()->sum('percentage');
-            }
-
-            factory(Category::class)->create([
-                'contest_id' => $contest->id,
-                'percentage' => $percentage,
-                'status' => $status,
-            ]);
-        }
-
-        foreach ($contest->categories()->get() as $category) {
-            foreach (range(1, $criteriaPerCategoryCount) as $i) {
-                $percentage = $faker->numberBetween(1, floor(100 / $criteriaPerCategoryCount));
-
-                if ($i === $criteriaPerCategoryCount) {
-                    $percentage = 100 - $category->criterias()->sum('percentage');
-                }
-
-                factory(Criteria::class)->create([
-                    'category_id' => $category->id,
-                    'percentage' => $percentage,
-                ]);
-            }
-
-            foreach ($contest->contestants()->get() as $contestant) {
-                $category->categoryContestants()->create([
-                    'contestant_id' => $contestant->id,
-                ]);
-            }
-
-            foreach ($contest->judges()->get() as $judge) {
-                $categoryJudge = $category->categoryJudges()->create([
-                    'judge_id' => $judge->id,
-                    'completed' => $category->status === 'done' ? 1 : 0,
-                ]);
-
-                foreach ($category->categoryContestants()->get() as $categoryContestant) {
-                    $categoryScore = $categoryJudge->categoryScores()->create([
-                        'category_id' => $category->id,
-                        'category_contestant_id' => $categoryContestant->id,
-                    ]);
-
-                    foreach ($category->criterias()->get() as $criteria) {
-                        $categoryScore->criteriaScores()->create([
-                            'criteria_id' => $criteria->id,
-                            'score' => $category->status === 'que' ? 0 : $faker->numberBetween(1, $criteria->percentage),
-                        ]);
-                    }
-                }
-            }
-        }
-
-        return;
-
-        $contest->load([
-            'contestants',
-            'judges',
-            'categories',
-            'categories.criterias',
-            'categories.categoryContestants',
-            'categories.categoryJudges',
-            'categories.categoryJudges.categoryScores',
-            'categories.categoryJudges.categoryScores.criteriaScores',
-            'categories.categoryScores',
-            'categories.categoryScores.criteriaScores',
-        ]);
-
-        dd($contest->toArray());
+        $contest->delete();
     }
 }
