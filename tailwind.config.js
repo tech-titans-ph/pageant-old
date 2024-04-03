1bad951afd8420d8ed581573774d97d7f68f598b
maganasamuel <maganasamuel001@gmail.com>
2024-04-03 11:58:00 +0800

    Use figtree font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   purge: ['./resources/js/components/**/*.vue', './resources/views/**/*.blade.php'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Figtree'],
+      },
+    },
   },
   variants: {},
   plugins: [require('@tailwindcss/custom-forms')],
