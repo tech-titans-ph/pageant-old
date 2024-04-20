ac6cb02654e407acae445c66467ac1495c737f36
Samuel Magana <maganasamuel001@gmail.com>
2024-04-20 22:00:11 +0800

    Relayout list of categories

--- resources/js/components/JudgeCategory.vue
+++ resources/js/components/JudgeCategory.vue
@@ -1,19 +1,19 @@
 <template>
-    <div v-if="!loading">
+    <div v-if="!loading" class="divide-y">
         <template v-if="categories.length">
             <a v-for="category in categories" :key="category.id"
                 :href="category.status === 'que' ? 'javascript:void(0);' : category.url"
-                :class="[category.class, 'block border-t p-6']">
+                :class="[category.class, 'block px-6 py-4 space-y-2']">
                 <div class="flex">
-                    <h1 class="flex-1 mb-4 text-lg font-bold">{{ category.name }}</h1>
-                    <div class="flex-1 text-right">
+                    <h1 class="flex-grow text-lg font-bold">{{ category.name }}</h1>
+                    <div class="flex-no-wrap flex-shrink text-right">
                         <span class="text-xl font-bold">{{ category.max_points_percentage }} {{ category.unit }}</span>
                     </div>
                 </div>
                 <div class="italic font-medium">{{ category.title }}</div>
             </a>
         </template>
-        <div v-else class="p-6 border-t">There are no available categories for you in this contest.</div>
+        <div v-else class="px-6 py-4">There are no available categories for you in this contest.</div>
     </div>
 </template>
 <script>
