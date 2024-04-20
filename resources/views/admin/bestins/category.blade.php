2501a5c099ec5a4c5c5bf0e5ae65f91a6cb1bfa8
Samuel Magana <maganasamuel001@gmail.com>
2024-04-20 13:44:32 +0800

    Relayout printing of bestins

---
+++ resources/views/admin/bestins/category.blade.php
@@ -0,0 +1,42 @@
+@if ($category->max_points_percentage)
+  <div class="font-bold">{{ $category->max_points_percentage }} {{ $contest->scoring_system == 'average' ? '%' : 'points' }}</div>
+@endif
+
+<table class="w-full mt-4">
+  <thead>
+    <tr>
+      <th class="px-2 py-1 border"
+        colspan="2">{{ Str::plural('Contestant', $category->ranked_contestants->count()) }}</th>
+
+      @foreach ($category->judges as $judge)
+        <th class="px-2 py-1 border">{{ $judge->name }}</th>
+      @endforeach
+
+      <th class="px-2 py-1 border">Average</th>
+    </tr>
+  </thead>
+  <tbody>
+    @foreach ($category->ranked_contestants as $contestant)
+      <tr class="{{ $loop->first ? 'bg-green-100' : '' }}"
+        style="page-break-inside: avoid;">
+        <td class="px-2 py-1 text-center align-middle border whitespace-no-wrap {{ $loop->first ? 'font-bold text-xs' : '' }}">
+          <div>Top {{ $contestant->ranking }}</div>
+        </td>
+        <td class="px-2 py-1 text-left border {{ $loop->first ? 'font-bold text-xs' : '' }}">
+          <div># {{ $contestant->order }} - {{ $contestant->name }}</div>
+          <div>{{ $contestant->alias }}</div>
+        </td>
+
+        @foreach ($category->judges as $judgeKey => $judge)
+          @php
+            $judgeScore = $contestant->judge_scores[$judge->pivot->id] ?? null;
+          @endphp
+
+          <td class="px-2 py-1 text-center border">{{ round($judgeScore['points_percentage'] ?? 0, 4) }}</td>
+        @endforeach
+
+        <td class="px-2 py-1 text-center border">{{ round($contestant->average, 4) }}</td>
+      </tr>
+    @endforeach
+  </tbody>
+</table>
