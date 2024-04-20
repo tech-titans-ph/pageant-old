83fd4a0e79c5dd13213ce1590c8a3bba347e58d3
Samuel Magana <maganasamuel001@gmail.com>
2024-04-20 18:06:00 +0800

    Relayout printer footer

--- resources/views/admin/print-footer.blade.php
+++ resources/views/admin/print-footer.blade.php
@@ -1,62 +1,72 @@
-<div class="m-10 space-y-10">
-  <p class="italic text-center" style="page-break-before: always;">***Nothing Follows***</p>
+<div class="m-3 text-xs">
+  <p class="italic text-center">***Nothing Follows***</p>
+  <br />
   <div class="flex justify-between"
     style="page-break-inside: avoid;">
     <div class="flex-shrink-1">
       <div>Tabulated By:</div>
-      <div class="px-4 py-1 mt-10 text-center border-t-2">
+      <br />
+      <div class="px-4 py-1 text-center border-t border-black">
         <div class="font-bold">NAME</div>
         <div>{{ Str::title(config('app.company')) }}</div>
       </div>
     </div>
-    <div class="w-1/3">&nbsp;</div>
     <div class="flex-shrink-1">
       <div>In witness thereof:</div>
-      <div class="px-4 py-1 mt-10 text-center border-t-2">
+      <br />
+      <div class="px-4 py-1 text-center border-t border-black">
         <div class="font-bold">NAME</div>
         <div>{{ Str::title(config('app.witness')) }}, Representative</div>
       </div>
     </div>
   </div>
-  <div class="space-y-10"
-    style="page-break-inside: avoid;">
+  <br />
+  <div style="page-break-inside: avoid;">
     <div>
-      <div class="mb-10">Approved By:</div>
-      <div class="grid grid-cols-2 gap-8 justify-items-center">
+      <div>Approved By:</div>
+      <br />
+      <div class="grid grid-cols-3 gap-4 place-items-center justify-items-center">
         @foreach ($model->judges as $judge)
           <div>
-            <div class="font-bold text-center uppercase">{{ $judge->name }}</div>
-            <div class="px-8 py-1 text-center border-t-2">Board of Judges</div>
+            <div class="px-4 font-bold text-center uppercase border-b border-black">{{ $judge->name }}</div>
+            <div class="text-center">Board of Judges</div>
           </div>
         @endforeach
       </div>
     </div>
-    <div class="flex justify-center">
-      <div class="px-8 py-1 text-center border-t-2">Head Coordinator</div>
+    <br />
+    <br />
+    <div class="flex justify-center ">
+      <div class="px-16 text-center border-t border-black">Head Coordinator</div>
     </div>
   </div>
-  <div class="space-y-10"
-    style="page-break-inside: avoid;">
+  <br />
+  <div style="page-break-inside: avoid;">
     <table>
       <tr>
-        <td class="px-2 py-1">Date Generated:</td>
-        <td class="px-2 border-b">{{ now()->format('m/d/Y') }}</td>
+        <td class="px-2">Date Generated:</td>
+        <td class="px-2 border-b border-black">{{ now()->format('m/d/Y') }}</td>
       </tr>
       <tr>
-        <td class="px-2 py-1">Time Generated:</td>
-        <td class="px-2 border-b">{{ now()->format('h:i A') }}</td>
+        <td class="px-2">Time Generated:</td>
+        <td class="px-2 border-b border-black">{{ now()->format('h:i A') }}</td>
       </tr>
     </table>
-    <div class="font-bold text-center text-red-600">
-      <p>NOTE: This is the official tabulation of the {{ $model->name }}. Erasures with the use of the any medium is prohibited.</p>
-      <p>This paper must not be tampered; therefore, any amendment is not
-        allowed. Strictly, this is the final result.</p>
-    </div>
+  </div>
+  <br />
+  <div class="font-bold text-center text-red-600"
+    style="page-break-inside: avoid;">
+    <p>NOTE: This is the official tabulation of the {{ $model->name }}. Erasures with the use of the any medium is prohibited.</p>
+    <p>This paper must not be tampered; therefore, any amendment is not
+      allowed. Strictly, this is the final result.</p>
+  </div>
+  <br />
+  <div style="page-break-inside: avoid;">
     <div class="flex items-center justify-center">
       <div class="text-xl">&copy;</div>
       <img src="{{ asset('images/' . env('LOGO_TEXT_BELOW')) }}"
-        class="h-40">
+        class="h-24">
     </div>
-    <div class="text-center">{{ now()->format('Y') }}</div>
+    <div class="mt-1 text-center">{{ now()->format('Y') }}</div>
   </div>
 </div>
