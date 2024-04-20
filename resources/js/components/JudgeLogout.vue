d7e7c37f354d69f3888bc7fd7accfcd69311bc9d
Samuel Magana <maganasamuel001@gmail.com>
2024-04-20 21:59:54 +0800

    Update spacing of logo dropdown

--- resources/js/components/JudgeLogout.vue
+++ resources/js/components/JudgeLogout.vue
@@ -5,7 +5,7 @@
             <img :src="logoUrl" class="h-16 mx-auto">
         </div>
         <div v-show="open"
-            class="absolute right-0 z-10 w-56 mt-2 origin-top-right bg-white rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+            class="absolute right-0 z-10 w-56 mt-1 mr-1 origin-top-right bg-white rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
             role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabindex="-1"
             v-click-outside="hideDropdown">
             <div class="py-1" role="none">
